526e6701902442f9e0e0d06473b0c517b8721e31
sandeep <plsandeep15@gmail.com>
2015-09-02 16:13:57 +0530

    Second comit

---
+++ GitDemoProject/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+		
+
+	}
+	
+	
+	public static void displayGreeting(){
+		System.out.println("Hellow happy sales people");
+		System.out.println(" This app shows sales data");
+	}
+
+}
---
+++ GitDemoProject/src/SalesData.java
@@ -0,0 +1,8 @@
+
+public class SalesData {
+	
+	public void display(){
+		System.out.println("Display salesdata");
+	}
+
+}
